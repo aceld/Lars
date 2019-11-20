8dbba2e8c48406fd9a2e5ce4847f2f4f3fa6bcba
aceld <danbing.at@gmail.com>
2019-11-20 11:31:46 +0800

    添加针对ID_ReportRequest的路由消息处理业务

--- lars_loadbalance_agent/src/agent_udp_server.cpp
+++ lars_loadbalance_agent/src/agent_udp_server.cpp
@@ -26,13 +26,20 @@ static void get_host_cb(const char *data, uint32_t len, int msgid, net_connectio
     //调用route_lb的获取host方法，得到rsp返回结果
     ptr_route_lb->get_host(modid, cmdid, rsp);
 
-
-
     //打包回执给api消息
     std::string responseString; 
     rsp.SerializeToString(&responseString);
     net_conn->send_message(responseString.c_str(), responseString.size(), lars::ID_GetHostResponse);
-    printf("rspstring size = %d\n", responseString.size());
+}
+
+static void report_cb(const char *data, uint32_t len, int msgid, net_connection *net_conn, void *user_data)
+{
+    lars::ReportRequest req;
+    
+    req.ParseFromArray(data, len);
+
+    route_lb *ptr_route_lb = (route_lb*)user_data;
+    ptr_route_lb->report_host(req);
 }
 
 
@@ -45,8 +52,11 @@ void * agent_server_main(void * args)
 
     udp_server server(&loop, "0.0.0.0", port);
 
-    //给server注册消息分发路由业务, 每个udp拥有一个对应的route_lb
+    //给server注册消息分发路由业务, 针对ID_GetHostRequest处理 每个udp拥有一个对应的route_lb
     server.add_msg_router(lars::ID_GetHostRequest, get_host_cb,  r_lb[port-8888]);
+
+    //给server注册消息分发路由业务，针对ID_ReportRequest处理
+    server.add_msg_router(lars::ID_ReportRequest, report_cb, r_lb[port-8888]);
 
 
     printf("agent UDP server :port %d is started...\n", port);
